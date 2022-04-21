784035af782c596de8f9a9ceef3dd649532a24d7
Juliet <agutu@gmail.com>
2022-04-21 23:28:11 +0300

    create book appointment form

--- docappointment/urls.py
+++ docappointment/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('', views.index, name='index'),
     path('accounts/register/', views.register, name='register'),
     path('profile', views.profile, name='profile'),
     path('edit-profile', views.editProfile, name='edit-profile'),
