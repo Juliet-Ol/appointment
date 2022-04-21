89bfe767da03d958daa8c3472b57533ceefcad1e
Juliet <agutu@gmail.com>
2022-04-21 16:13:27 +0300

    add appointment url path

--- docappointment/urls.py
+++ docappointment/urls.py
@@ -1,4 +1,6 @@
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -7,4 +9,8 @@ urlpatterns = [
     path('accounts/register/', views.register, name='register'),
     path('profile', views.profile, name='profile'),
     path('edit-profile', views.editProfile, name='edit-profile'),
+    path('appointment', views.appointment, name='appointment'),
+
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
