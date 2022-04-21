180532a33245fc1b793e77d1177d5a25040ebeeb
Juliet <agutu@gmail.com>
2022-04-21 14:35:57 +0300

    add profile model for patients

--- docappointment/models.py
+++ docappointment/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50) 
+    identiy_number = models.CharField(max_length=50, blank=True, primary_key=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(null=True) 
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default='')
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    bio = models.TextField(max_length=300, default='') 
+
 
 
